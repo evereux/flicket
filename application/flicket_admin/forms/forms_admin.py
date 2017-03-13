4704d38479be84894dab0d6fecabe077b4776ded
evereux <evereux@gmail.com>
2017-03-13 17:34:32 +0000

    Fixed sqlalchemy 500 error when trying to edit/add users with already existing email address. Removed redundant forms_main.py file.

--- application/flicket_admin/forms/forms_admin.py
+++ application/flicket_admin/forms/forms_admin.py
@@ -6,14 +6,30 @@
 import bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectMultipleField, PasswordField, HiddenField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, EqualTo
 
+from application.flicket.scripts.functions_login import check_email_format
 from application.flicket.models.flicket_user import (FlicketGroup,
                                                      FlicketUser,
                                                      username_maxlength,
                                                      name_maxlength,
                                                      email_maxlength,
                                                      group_maxlength)
+
+
+def does_username_exist(form, field):
+    """
+    Username must be unique so we check against the database to ensure it doesn't
+    :param form:
+    :param field:
+    :return True / False:
+    """
+    result = FlicketUser.query.filter_by(username=form.username.data).count()
+    if result > 0:
+        field.errors.append('A user with this username has already registered.')
+        return False
+
+    return True
 
 
 def group_exists(form, field):
@@ -31,13 +47,68 @@ def group_exists(form, field):
 
 
 def check_password(form, field):
-    """ Is users password correct. """
-    ok = True
+    """
+    Check formatting of password.
+    :param form:
+    :param field:
+    :return True / False:
+    """
     result = FlicketUser.query.filter_by(id=form.id.data).first()
-
     if bcrypt.hashpw(form.password.data.encode('utf-8'), result.password) != result.password:
         field.errors.append('Entered password is incorrect.')
         return False
+    return True
+
+
+def check_password_formatting(form, field):
+    """
+    Check formatting of password.
+    :param form:
+    :param field:
+    :return True / False:
+    """
+    ok = True
+    _min = 6
+    if len(field.data) < _min:
+        field.errors.append('Password must be more than {} characters.'.format(min))
+        ok = False
+    if not any(s.isupper() for s in field.data) and not any(s.islower() for s in field.data):
+        field.errors.append('Password must contain upper and lower characters.')
+        ok = False
+
+    return ok
+
+
+def check_email(form, field):
+    """
+    Checks formatting of email and also checks that a user is not already registered with the same email address
+    :param form:
+    :param field:
+    :return:
+    """
+    ok = True
+    if not check_email_format(field.data):
+        field.errors.append('Please enter a correctly formatted email address.')
+        ok = False
+    result = FlicketUser.query.filter_by(email=form.email.data).count()
+    if result > 0:
+        field.errors.append('A user with this email address is already registered.')
+        ok = False
+
+    return ok
+
+
+class RegisterForm(FlaskForm):
+    """ Register user form. """
+    username = StringField('username', validators=[Length(min=4, max=username_maxlength), does_username_exist])
+    name = StringField('name', validators=[Length(min=4, max=name_maxlength)])
+    email = StringField('email', validators=[Length(min=5, max=email_maxlength), check_email])
+    password = PasswordField('password', validators=[
+        DataRequired(),
+        EqualTo('confirm', message='Passwords must match'),
+        check_password_formatting
+    ])
+    confirm = PasswordField('Repeat Password')
 
 
 class EditUserForm(FlaskForm):
@@ -47,7 +118,7 @@ class EditUserForm(FlaskForm):
 
     username = StringField('username', validators=[Length(min=4, max=username_maxlength)])
     name = StringField('name', validators=[Length(min=4, max=name_maxlength)])
-    email = StringField('email', validators=[Length(min=5, max=email_maxlength)])
+    email = StringField('email', validators=[Length(min=5, max=email_maxlength), check_email])
     groups = SelectMultipleField('groups', coerce=int)
 
 
