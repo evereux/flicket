42706634641bb983c3c1b85efa711806ddf4b356
evereux <evereux@gmail.com>
2017-03-14 19:00:52 +0000

    Updated index page statistics. Now split into departments.

--- application/flicket/views/index.py
+++ application/flicket/views/index.py
@@ -4,6 +4,7 @@
 # Flicket - copyright Paul Bourne: evereux@gmail.com
 
 import datetime
+import pprint
 
 from flask import render_template, g
 from flask_login import login_required
@@ -33,14 +34,36 @@ def index():
     # initialise base query
     query = FlicketTicket.query
     total = FlicketTicket.query.count()
-    in_work = query.filter(FlicketTicket.current_status.has(FlicketStatus.status == s_wip)).count()
-    _open = query.filter(FlicketTicket.current_status.has(FlicketStatus.status == s_open)).count()
     total_days = query.filter(FlicketTicket.date_added > days_obj).count()
+
+    # get list of statuses
+    statuses = [({'id': s.id, 'status': s.status}, {}) for s in FlicketStatus.query.order_by(FlicketStatus.status).all()]
+    # find number of tickets for each status
+    for s in statuses:
+        ticket_num = query.filter(FlicketTicket.current_status.has(FlicketStatus.id == s[0]['id'])).count()
+        s[1]['ticket_num'] = ticket_num
+
+    # get list of departments
+    departments = [({'id': d.id, 'department': d.department}, []) for d in FlicketDepartment.query.all()]
+
+    # department_filter = FlicketDepartment.query.filter_by(department=department).first()
+    # tickets = tickets.filter(FlicketTicket.category.has(FlicketCategory.department == department_filter))
+
+    # find number of tickets for each department based on status
+    for d in departments:
+        for s in statuses:
+            department_filter = query.filter(FlicketTicket.category.has(FlicketCategory.department_id == d[0]['id']))
+            ticket_num = department_filter.filter(FlicketTicket.current_status.has(FlicketStatus.id == s[0]['id'])).count()
+            d[1].append(({'status': s[0]['status']}, {'total_num': ticket_num}))
+
+    pp = pprint.PrettyPrinter(indent=4)
+
+    pp.pprint(departments)
 
     return render_template('flicket_index.html',
                            title='Flicket',
                            total=total,
-                           in_work=in_work,
-                           _open=_open,
                            total_days=total_days,
-                           days=days)
+                           days=days,
+                           statuses=statuses,
+                           departments=departments)
