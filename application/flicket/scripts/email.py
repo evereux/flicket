e5ccef887c6d115caa91dd0a4f5ed7df8d43e1ba
evereux <evereux@gmail.com>
2017-04-16 17:41:06 +0100

    Correct utf-8 declaration in .py files.

--- application/flicket/scripts/email.py
+++ application/flicket/scripts/email.py
@@ -1,5 +1,5 @@
 #! usr/bin/python3
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 #
 # Flicket - copyright Paul Bourne: evereux@gmail.com
 
--- scripts/create_json.py
+++ scripts/create_json.py
@@ -1,5 +1,5 @@
 #! usr/bin/python3
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 
 from getpass import getpass
