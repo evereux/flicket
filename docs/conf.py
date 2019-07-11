09d1bb97d1013127cfa5ccb9460a32071b59cbdd
evereux <evereux@gmail.com>
2019-07-11 12:29:15 +0100

    attempt to get api documented correctly at readthedocs. fine locally.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../application'))
+# sys.path.insert(0, os.path.abspath('../application'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
