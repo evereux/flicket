11890548d9d787f116d6d1b54d427acc20f13848
evereux <=>
2019-12-13 10:04:41 +0000

    new branch 0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, evereux@gmail.com'
 author = 'evereux@gmail.com'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.2'
 
 # -- General configuration ---------------------------------------------------
 
