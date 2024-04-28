b310e17650277a5872543f3ad1d5a6acdb7b449c
evereux <evereux@gmail.com>
2024-04-28 15:18:17 +0100

    fixed bug on submitting config changes to a SQLite database
    see issue #76

--- application/flicket_admin/views/view_config.py
+++ application/flicket_admin/views/view_config.py
@@ -31,34 +31,33 @@ def config():
 
         config_details.mail_server = form.mail_server.data
         config_details.mail_port = form.mail_port.data
-        config_details.mail_use_tls = form.mail_use_tls.data
-        config_details.mail_use_ssl = form.mail_use_ssl.data
-        config_details.mail_debug = form.mail_debug.data
+        config_details.mail_use_tls = form.mail_use_tls.data or False
+        config_details.mail_use_ssl = form.mail_use_ssl.data or False
+        config_details.mail_debug = form.mail_debug.data or False
         config_details.mail_username = form.mail_username.data
 
         config_details.mail_default_sender = form.mail_default_sender.data
         config_details.mail_max_emails = form.mail_max_emails.data
-        config_details.mail_suppress_send = form.mail_suppress_send.data
-        config_details.mail_ascii_attachments = form.mail_ascii_attachments.data
+        config_details.mail_suppress_send = form.mail_suppress_send.data or False
+        config_details.mail_ascii_attachments = form.mail_ascii_attachments.data or False
 
-        config_details.application_title = form.application_title.data,
-        config_details.posts_per_page = form.posts_per_page.data,
-        config_details.allowed_extensions = form.allowed_extensions.data,
+        config_details.application_title = form.application_title.data
+        config_details.posts_per_page = form.posts_per_page.data
+        config_details.allowed_extensions = form.allowed_extensions.data
         config_details.ticket_upload_folder = form.ticket_upload_folder.data
         config_details.base_url = form.base_url.data
 
         config_details.use_auth_domain = form.use_auth_domain.data
-        config_details.auth_domain = form.auth_domain.data
+        config_details.auth_domain = form.auth_domain.data or False
 
         config_details.csv_dump_limit = form.csv_dump_limit.data
 
-        config_details.change_category = form.change_category.data
+        config_details.change_category = form.change_category.data or False
         config_details.change_category_only_admin_or_super_user = form.change_category_only_admin_or_super_user.data
 
         # Don't change the password if nothing was entered.
         if form.mail_password.data != '':
             config_details.mail_password = form.mail_password.data
-
         db.session.commit()
         flash(gettext('Config details updated.'), category='success')
         return redirect(url_for('admin_bp.config'))
